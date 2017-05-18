77b814b04f40537efe6a853a536689ab051a639b
g1nc <ginc.xyz@gmail.com>
2017-05-19 01:20:07 +0300

    added search

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   namespace :api do
-    resources :events
+    resources :events, only: %i[index create] do
+      get :search, on: :collection
+    end
   end
+
   root 'dashboard#index'
 end
